f0b3e4746cf2c302bc886735eb6e1a4bbbf6ced3
Edmore Moyo <etmoyo@gmail.com>
2012-09-17 16:02:20 +0200

    Fix permission denied errors

--- init.rb
+++ init.rb
@@ -11,7 +11,7 @@ backup_directory = "/Users/#{user}/dotfiles_backup/"
 system("sudo mkdir #{backup_directory}")
 
 # Backup the current emacs dotfiles
-system("mv /Users/#{user}/.emacs #{backup_directory}")
-system("mv /Users/#{user}/.emacs.d/ #{backup_directory}/.emacs.d/")
+system("sudo mv /Users/#{user}/.emacs #{backup_directory}")
+system("sudo mv /Users/#{user}/.emacs.d/ #{backup_directory}/.emacs.d/")
 
 system("sudo ln -s #{target_directory}.emacs /Users/#{user}/.emacs")
