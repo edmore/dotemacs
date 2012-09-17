0d996509a01aa654128238fda915990af65a0285
Edmore Moyo <etmoyo@gmail.com>
2012-09-17 15:53:41 +0200

    Initial dotfiles commit

---
+++ .emacs
@@ -0,0 +1,4 @@
+(setq inhibit-startup-message t)
+(global-font-lock-mode t)
+(menu-bar-mode 0)
+(column-number-mode t)
---
+++ init.rb
@@ -0,0 +1,17 @@
+#! /usr/bin/ruby
+
+##########################################################
+# Description - Initialize my emacs configuration
+# Author - www.edmoremoyo.com
+##########################################################
+
+user = ENV['USER']
+target_directory = "/Users/#{user}/dotfiles/"
+backup directory = /Users/#{user}/dotfiles_backup/
+system("sudo mkdir #{backup_directory}")
+
+# Backup the current emacs dotfiles
+system("mv /Users/#{user}/.emacs #{backup_directory}")
+system("mv /Users/#{user}/.emacs.d/ #{backup_directory}/.emacs.d/")
+
+system("sudo ln -s #{target_directory}.emacs /Users/#{user}/.emacs")
