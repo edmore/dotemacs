7a9868a0e753371a80e1afc987d46944047beadb
Edmore Moyo <etmoyo@Moyo-Mac.(none)>
2012-09-19 06:22:05 +0200

    backup old emacs config if the files exist

--- bin/setup.rb
+++ bin/setup.rb
@@ -11,9 +11,9 @@ backup_directory = "#{home}/.emacs.d/"
 
 system("sudo mkdir #{backup_directory}")
 
-# Backup the current emacs dotfiles
-system("sudo mv #{home}/.emacs #{backup_directory}")
-system("sudo mv #{home}/.emacs.d/ #{backup_directory}/.emacs.d/")
+# Backup the current emacs dotfiles if they exist
+system("sudo mv #{home}/.emacs #{backup_directory}") if File.exist?("#{home}/.emacs")
+system("sudo mv #{home}/.emacs.d/ #{backup_directory}/.emacs.d/") if File.directory?("#{home}/.emacs.d/")
 
 # Rename dotfiles folder to .emacs.d
 system("sudo mv #{home}/dotfiles/ #{home}/.emacs.d/")
