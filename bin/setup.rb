aa9ac8ca0fd8bc39a5baedd46e98c75a906b9aed
Edmore Moyo <etmoyo@Moyo-Mac.(none)>
2012-09-19 06:38:04 +0200

    More changes to setup script

--- bin/setup.rb
+++ bin/setup.rb
@@ -6,14 +6,14 @@
 ##########################################################
 
 home = File.expand_path('~')
-target_directory = "#{home}/.emacs.d/"
-backup_directory = "#{home}/.emacs.d/"
+backup_directory = "#{home}/.emacs.d_backup/"
 
 system("sudo mkdir #{backup_directory}")
 
 # Backup the current emacs dotfiles if they exist
 system("sudo mv #{home}/.emacs #{backup_directory}") if File.exist?("#{home}/.emacs")
-system("sudo mv #{home}/.emacs.d/ #{backup_directory}/.emacs.d/") if File.directory?("#{home}/.emacs.d/")
+system("sudo mv #{home}/.emacs.d/ #{backup_directory}.emacs.d/") if File.directory?("#{home}/.emacs.d/")
 
 # Rename dotfiles folder to .emacs.d
 system("sudo mv #{home}/dotfiles/ #{home}/.emacs.d/")
+Dir.chdir(home)
