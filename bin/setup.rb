f66099a1f56ae32ba49c5f901a33767c50ee1d94
Edmore Moyo <etmoyo@gmail.com>
2012-09-28 13:26:01 +0200

    Only create folder if old files exist

--- bin/setup.rb
+++ bin/setup.rb
@@ -8,11 +8,13 @@
 home = File.expand_path('~')
 backup_directory = "#{home}/.emacs.d_backup/"
 cmds = []
+old_emacs_file = File.exist?("#{home}/.emacs")
+old_emacs_folder = File.directory?("#{home}/.emacs.d/")
 
-cmds << "sudo mkdir #{backup_directory}"
+cmds << "sudo mkdir #{backup_directory}" if (old_emacs_file || old_emacs_folder)
 # Backup the current emacs dotfiles if they exist
-cmds << "sudo mv #{home}/.emacs #{backup_directory}" if File.exist?("#{home}/.emacs")
-cmds << "sudo mv #{home}/.emacs.d/ #{backup_directory}.emacs.d/" if File.directory?("#{home}/.emacs.d/")
+cmds << "sudo mv #{home}/.emacs #{backup_directory}" if old_emacs_file
+cmds << "sudo mv #{home}/.emacs.d/ #{backup_directory}.emacs.d/" if old_emacs_folder
 # Rename dotfiles folder to .emacs.d
 cmds << "sudo mv #{home}/dotemacs/ #{home}/.emacs.d/"
 # update submodules
