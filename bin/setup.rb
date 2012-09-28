a31c4c21cbbcc70c46e456c9b5fb02083b65a0ad
Edmore Moyo <etmoyo@gmail.com>
2012-09-28 12:54:30 +0200

    Refactor setup script

--- bin/setup.rb
+++ bin/setup.rb
@@ -7,17 +7,16 @@
 
 home = File.expand_path('~')
 backup_directory = "#{home}/.emacs.d_backup/"
+cmds = []
 
-system("sudo mkdir #{backup_directory}")
-
+cmds << "sudo mkdir #{backup_directory}"
 # Backup the current emacs dotfiles if they exist
-system("sudo mv #{home}/.emacs #{backup_directory}") if File.exist?("#{home}/.emacs")
-system("sudo mv #{home}/.emacs.d/ #{backup_directory}.emacs.d/") if File.directory?("#{home}/.emacs.d/")
-
+cmds << "sudo mv #{home}/.emacs #{backup_directory}" if File.exist?("#{home}/.emacs")
+cmds << "sudo mv #{home}/.emacs.d/ #{backup_directory}.emacs.d/" if File.directory?("#{home}/.emacs.d/")
 # Rename dotfiles folder to .emacs.d
-system("sudo mv #{home}/dotemacs/ #{home}/.emacs.d/")
-
+cmds << "sudo mv #{home}/dotemacs/ #{home}/.emacs.d/"
 # update submodules
-system("git submodule update --init")
+cmds << "git submodule update --init"
 
+system cmds.join("&&")
 puts "Setup complete :-) , you may navigate to [ ~/.emacs.d/ ] to view your GNU Emacs config files."
