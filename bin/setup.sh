9c2ee2ab89094e58481058bea4f1ef18df0506ff
Edmore Moyo <etmoyo@gmail.com>
2012-12-31 22:59:59 +0200

    Refactoring setup file

--- bin/setup.sh
+++ bin/setup.sh
@@ -11,16 +11,16 @@ DATE=$(date +"%Y%m%d%H%M")
 if [ -e $HOME/.emacs -o -d $HOME/.emacs.d/ ]; then
     echo "Creating backup directory ..."
     sudo mkdir -p $BACKUP_DIR
-fi
 
-if [ -e $HOME/.emacs ]; then
-    echo "Backing up .emacs file ..."
-    sudo mv $HOME/.emacs $BACKUP_DIR/.emacs_$DATE
-fi
+    if [ -e $HOME/.emacs ]; then
+        echo "Backing up .emacs file ..."
+        sudo mv $HOME/.emacs $BACKUP_DIR/.emacs_$DATE
+    fi
 
-if [ -d $HOME/.emacs.d/ ]; then
-    echo "Backing up .emacs.d folder ..."
-    sudo mv $HOME/.emacs.d/ $BACKUP_DIR/.emacs.d_$DATE/
+    if [ -d $HOME/.emacs.d/ ]; then
+        echo "Backing up .emacs.d folder ..."
+        sudo mv $HOME/.emacs.d/ $BACKUP_DIR/.emacs.d_$DATE/
+    fi
 fi
 
 sudo mv $HOME/dotemacs/ $HOME/.emacs.d
