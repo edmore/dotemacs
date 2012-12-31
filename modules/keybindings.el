fd94ff2a9462ea10f6f7023a55abfba5423a7cfc
Edmore Moyo <etmoyo@gmail.com>
2012-12-31 10:34:20 +0200

    Add comments

--- modules/keybindings.el
+++ modules/keybindings.el
@@ -2,6 +2,7 @@
 
 ;; Goto-line short-cut key
 (global-set-key (kbd "C-l") 'goto-line)
+;; (inc|de)crease text size
 (global-set-key (kbd "C-=") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
 
