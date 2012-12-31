d4e14bd4ac83532b93c1fb8ffac4e5621ee181e4
Edmore Moyo <etmoyo@gmail.com>
2012-12-31 10:32:03 +0200

    Text (in|de)crease keybindings

--- modules/keybindings.el
+++ modules/keybindings.el
@@ -1,6 +1,8 @@
 ;; Emacs keybindings
 
 ;; Goto-line short-cut key
-(global-set-key "\C-l" 'goto-line)
+(global-set-key (kbd "C-l") 'goto-line)
+(global-set-key (kbd "C-=") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
 
 (provide 'keybindings)
