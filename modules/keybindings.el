d7b75862be328dd00e1c9210565d26052f87b17d
Edmore Moyo <etmoyo@gmail.com>
2012-12-31 11:35:05 +0200

    Commenting and uncommenting

--- modules/keybindings.el
+++ modules/keybindings.el
@@ -5,5 +5,7 @@
 ;; (inc|de)crease text size
 (global-set-key (kbd "C-=") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
+;; Commenting
+(global-set-key (kbd "C-x /") 'comment-or-uncomment-region)
 
 (provide 'keybindings)
