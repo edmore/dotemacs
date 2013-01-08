8deb1f2b3045d3bfd1c36bb1d2f545bd1316fedd
Edmore Moyo <etmoyo@gmail.com>
2013-01-08 12:45:43 +0200

    add some keybindings for switching between buffers

--- modules/keybindings.el
+++ modules/keybindings.el
@@ -7,5 +7,7 @@
 (global-set-key (kbd "C--") 'text-scale-decrease)
 ;; Commenting
 (global-set-key (kbd "C-x /") 'comment-or-uncomment-region)
-
+;; switching between buffers
+(global-set-key (kbd "C-<tab>") 'next-buffer)
+(global-set-key (kbd "C-S-<tab>") 'previous-buffer)
 (provide 'keybindings)
