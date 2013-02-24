33bb47e6f13aed2f1fa64a2781fdacce905bea07
Edmore Moyo <etmoyo@gmail.com>
2013-02-24 21:44:13 +0200

    ensure that .md files are opened in markdown mode

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
 ;; markdown mode
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 ;; Add Solarized theme
 (if window-system
