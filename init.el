9647798d37921a01a8656a63e8f53ee78c7098d4
Edmore Moyo <etmoyo@gmail.com>
2013-03-23 09:58:25 +0200

    add find-file-in-project

--- init.el
+++ init.el
@@ -20,6 +20,8 @@
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+;; find-file-in-project
+(require 'find-file-in-project)
 
 ;; Add Solarized theme
 (if window-system
